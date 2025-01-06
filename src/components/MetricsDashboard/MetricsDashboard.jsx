bdddddde17bccab65835ad319ac0bfc2eeb9d639
Dan Parry <dan.parry22@gmail.com>
2025-01-06 14:20:51 +0200

    metrics update

--- src/components/MetricsDashboard/MetricsDashboard.jsx
+++ src/components/MetricsDashboard/MetricsDashboard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { metricsData } from '../../data/metricsData.js';
 import styled from 'styled-components';
+import metricsImage from '../../assets/metrics.png';
 
 const MetricsContainer = styled.div`
   @apply bg-white rounded-lg shadow-md p-4;
@@ -11,7 +10,7 @@ const MetricsDashboard = () => {
   return (
     <MetricsContainer>
       <h2 className="font-bold text-lg">Metrics and Analytics</h2>
-      <Line data={metricsData} />
+      <img src={metricsImage} alt="Metrics Visualization" />
     </MetricsContainer>
   );
 };
